59e5c6d19c0084b0a2aa8f4f8aa8b0fbb6d4777e
KishoreKelam <148374711+KishoreKelam@users.noreply.github.com>
2024-05-13 10:11:10 -0500

    Java features test demo

---
+++ src/main/java/com/models/Alien.java
@@ -0,0 +1,43 @@
+package com.models;
+
+
+public class Alien {
+    
+    private int aid;
+    private String aname;
+    private String tech;
+
+    public Alien() {
+        super();
+        System.out.println("Object created");
+    }
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
+    }
+}
